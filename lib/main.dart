b29538df90052f4a54a651ca1ab40ba01fdb2b33
Aleksandr Kelbas <aleksandr.kelbas@ukfast.co.uk>
2018-10-26 13:50:24 +0100

    added product manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import './product_manager.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('EasyList'),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('EasyList'),
       ),
-    );
+      body: ProductManager(),
+    ));
   }
 }
