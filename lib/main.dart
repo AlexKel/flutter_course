b4c75de9f5f4c08894e321751e8b59a57a8a3082
Aleksandr Kelbas <aleksandr.kelbas@ukfast.co.uk>
2019-04-08 16:24:52 +0100

    course section finished

--- lib/main.dart
+++ lib/main.dart
@@ -37,9 +37,11 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          primarySwatch: Colors.deepOrange,
-          accentColor: Colors.deepPurple,
-          brightness: Brightness.light),
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple,
+        buttonColor: Colors.deepPurple,
+      ),
       // home: AuthPage(),
       routes: {
         '/': (BuildContext context) => AuthPage(),
@@ -57,7 +59,10 @@ class _MyAppState extends State<MyApp> {
           final int index = int.parse(pathElements[2]);
           return MaterialPageRoute<bool>(
               builder: (BuildContext context) => ProductPage(
-                  _products[index]['title'], _products[index]['image']));
+                  _products[index]['title'],
+                  _products[index]['image'],
+                  _products[index]['price'],
+                  _products[index]['description']));
         }
 
         return null;
--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -12,69 +12,86 @@ class _AuthPageState extends State<AuthPage> {
   String _passwordValue;
   bool _termsAccepted = false;
 
+  DecorationImage _buildBackgroundImage() {
+    return DecorationImage(
+        fit: BoxFit.cover,
+        colorFilter:
+            ColorFilter.mode(Colors.black.withOpacity(0.5), BlendMode.dstATop),
+        image: AssetImage('assets/background.jpg'));
+  }
+
+  Widget _buildEmailTextField() {
+    return TextField(
+      decoration: InputDecoration(
+          labelText: 'Email', filled: true, fillColor: Colors.white),
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (String value) {
+        setState(() {
+          _emailValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildPasswordTextField() {
+    return TextField(
+      decoration: InputDecoration(
+          labelText: 'Password', filled: true, fillColor: Colors.white),
+      obscureText: true,
+      onChanged: (String value) {
+        setState(() {
+          _passwordValue = value;
+        });
+      },
+    );
+  }
+
+  Widget _buildAcceptSwitch() {
+    return SwitchListTile(
+      value: _termsAccepted,
+      onChanged: (bool value) {
+        setState(() {
+          _termsAccepted = value;
+        });
+      },
+      title: Text('Accept terms'),
+    );
+  }
+
+  _submitForm() {
+    print(_emailValue);
+    print(_passwordValue);
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double deviceWidth = MediaQuery.of(context).size.width;
+    final double targetWidth = deviceWidth > 768.0 ? 500.0 : deviceWidth * 0.95;
+
     return Scaffold(
         appBar: AppBar(title: Text('Login')),
         body: Container(
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                        Colors.black.withOpacity(0.5), BlendMode.dstATop),
-                    image: AssetImage('assets/background.jpg'))),
+            decoration: BoxDecoration(image: _buildBackgroundImage()),
             padding: EdgeInsets.all(10.0),
             child: Center(
                 child: SingleChildScrollView(
-                    child: Column(
-              children: <Widget>[
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Email',
-                      filled: true,
-                      fillColor: Colors.white),
-                  keyboardType: TextInputType.emailAddress,
-                  onChanged: (String value) {
-                    setState(() {
-                      _emailValue = value;
-                    });
-                  },
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      filled: true,
-                      fillColor: Colors.white),
-                  obscureText: true,
-                  onChanged: (String value) {
-                    setState(() {
-                      _passwordValue = value;
-                    });
-                  },
-                ),
-                SwitchListTile(
-                  value: _termsAccepted,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _termsAccepted = value;
-                    });
-                  },
-                  title: Text('Accept terms'),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                RaisedButton(
-                    color: Theme.of(context).accentColor,
-                    textColor: Colors.white,
-                    child: Text('LOGIN'),
-                    onPressed: () {
-                      print(_emailValue);
-                      print(_passwordValue);
-                      Navigator.pushReplacementNamed(context, '/products');
-                    })
-              ],
-            )))));
+                    child: Container(
+                        width: targetWidth,
+                        child: Column(
+                          children: <Widget>[
+                            _buildEmailTextField(),
+                            SizedBox(height: 10.0),
+                            _buildPasswordTextField(),
+                            _buildAcceptSwitch(),
+                            SizedBox(
+                              height: 10.0,
+                            ),
+                            RaisedButton(
+                                textColor: Colors.white,
+                                child: Text('LOGIN'),
+                                onPressed: _submitForm)
+                          ],
+                        ))))));
   }
 }
